d8e0cda2893f535f89ce80f75a2e12cfae24610c
gastonprieto <gastonprietoutn@gmail.com>
2016-08-27 11:28:38 -0300

    primer mapeo

--- src/main/java/jacklow/model/Vehiculo.java
+++ src/main/java/jacklow/model/Vehiculo.java
@@ -1,8 +1,22 @@
 package jacklow.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vehiculos")
 public class Vehiculo {
+	
+	@Id @GeneratedValue
+	private int id;
+	
 	private String vtu;
 	private String patente;
+	
+	@SuppressWarnings("unused")
+	private Vehiculo() {}
 
 	public Vehiculo(String vtu, String patente) {
 		super();
@@ -16,6 +30,10 @@ public class Vehiculo {
 
 	public String getPatente() {
 		return patente;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 }
--- src/test/java/db/ContextTest.java
+++ src/test/java/db/ContextTest.java
@@ -1,8 +1,14 @@
 package db;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import jacklow.model.Vehiculo;
 
+import javax.persistence.EntityManager;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
@@ -16,6 +22,25 @@ public class ContextTest extends AbstractPersistenceTest implements WithGlobalEn
 	@Test
 	public void contextUpWithTransaction() throws Exception {
 		withTransaction(() -> {});
+	}
+	
+	@Test
+	public void getDelVehiculo() {
+		EntityManager entityManager = 
+				PerThreadEntityManagers.
+				getEntityManager();
+		
+		Vehiculo vehiculo = entityManager.find(Vehiculo.class, 1);
+		assertEquals(1, vehiculo.getId());
+		assertEquals("unaVtu", vehiculo.getVtu());
+		assertEquals("unaPatente", vehiculo.getPatente());
+		
+		Vehiculo otroVehiculo = entityManager
+				.find(Vehiculo.class, 1);
+		Assert.assertTrue(vehiculo == 
+				otroVehiculo);
+		
+		
 	}
 
 }
